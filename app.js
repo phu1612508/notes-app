698b9c140ad54574f8368ff0582621a284ca5354
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-09-02 21:47:25 +0700

    Argument Parsing with Yargs

--- app.js
+++ app.js
@@ -1,3 +1,47 @@
 const chalk = require('chalk');
 const yargs =require('yargs');
 const getNotes= require("./notes.js");
+
+yargs.command({
+    command:"add",
+    describe:"add a note",
+    builder: {
+        title:{
+            desc:"Note title"
+        },
+        body:{
+            desc:"body",
+            demandOption:true,
+            type:"String"
+        }
+    },
+    handler: function (argv) {
+        console.log("Title:",argv.title);
+        console.log("Body:",argv.body);
+    }
+});
+
+yargs.command({
+    command:"remove",
+    describe:"remove a note",
+    handler: function(){
+        console.log("Removing a note");
+    }
+});
+
+yargs.command({
+    command:"list",
+    describe:"list notes",
+    handler: function(){
+        console.log("List notes");
+    }
+});
+
+yargs.command({
+    command:"read",
+    describe:"read a note",
+    handler: function(){
+        console.log("Read a note");
+    }
+});
+console.log(yargs.argv);
